0699b67b6f6a43da00ad4667209ab9ab430efcc9
Moises <138265949+moiseshxs@users.noreply.github.com>
2024-01-16 11:15:44 -0300

    Update stack.js

--- src/routes/stack.js
+++ src/routes/stack.js
@@ -1,26 +1,45 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-import Home from '../pages/Home';
+//import Home from '../pages/Home';
 import Login from '../pages/Login';
+import Config from '../pages/Config';
 
+import Tabs from './tabs';
 const Stack = createNativeStackNavigator();
 
 export default function StackRoutes() {
   return (
-    <Stack.Navigator initialRouteName='Home' screenOptions={{headerShown: true}}>
-      <Stack.Screen 
-        name='Home'
-        component={Home}
-        options={{
-          
-        }}
-      />
-      
+    <Stack.Navigator initialRouteName='Login' screenOptions={{headerShown: true}}>
       <Stack.Screen 
         name='Login'
         component={Login}
         options={{
-          
+          title: 'Login',
+          headerStyle: { backgroundColor: 'purple' },
+          headerTintColor: 'white',
+          headerShown: false,
+        }}
+      />
+      
+      <Stack.Screen 
+        name='Home'
+        component={Tabs}
+        options={{
+          title: 'Inicio',
+          headerStyle: { backgroundColor: 'white' },
+          headerTintColor: 'black',
+          headerShown: false,
+        }}
+      />
+
+      <Stack.Screen 
+        name='Config'
+        component={Config}
+        options={{
+          title: 'Configurações',
+          headerStyle: { backgroundColor: 'white' },
+          headerTintColor: 'black',
+          headerShown: true,
         }}
       />
     </Stack.Navigator>
