f6339bae94cb76b93a35d597e33add5d46fbc414
Moises <138265949+moiseshxs@users.noreply.github.com>
2024-01-16 11:18:37 -0300

    Update index.js

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Text, View, SafeAreaView} from 'react-native';
+import { Text, View, SafeAreaView, TouchableOpacity} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import styles from './styles';
 
+
+
 export default function Login() {
+    const navigation = useNavigation();
+
     return (
         <SafeAreaView style={styles.container}>
-            <View><Text>Login</Text></View>
+            <View>
+                <TouchableOpacity style={styles.botao} onPress={() => navigation.navigate('Home')}>
+                    <Text>Logi</Text>
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     );
 }
