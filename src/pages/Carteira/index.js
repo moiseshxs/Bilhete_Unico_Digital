40aa4a4616e8cc554553b978364436f15a51bc5e
Moises <moiseshenryb@gmail.com>
2024-01-18 11:20:01 -0300

    Carteira

--- src/pages/Carteira/index.js
+++ src/pages/Carteira/index.js
@@ -1,20 +1,114 @@
-import React from 'react';
-import { Text, View, SafeAreaView, ScrollView, StatusBar} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { Text, View, Image, FlatList, ScrollView, SafeAreaView, StatusBar, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import styles from './styles';
 
-export default function Carteira() {
-    return (
-        <SafeAreaView style={styles.container}>
-            <ScrollView>
-                <View style={styles.nav}>
-                    <Text>Carteira</Text>
-                </View>
+const DATA = [
+  {
+    id: '1',
+    passagem: 'Compra no PIX',
+    linha: '2 PASSAGENS',
+    data: '18 OUT ',
+  },
+  {
+    id: '2',
+    passagem: 'Compra no PIX',
+    linha: '20 PASSAGENS',
+    data: '10 OUT',
+  },
+  {
+    id: '3',
+    passagem: 'Compra no PIX',
+    linha: '15 PASSAGENS',
+    data: '24 SET',
+  },
+  {
+    id: '4',
+    passagem: 'Compra no PIX',
+    linha: '10 PASSAGENS',
+    data: '4 SET',
+  },
+];
 
-                <View style={styles.recarga}>
-                    <Text>Carteira</Text>
+const Item = ({passagem,linha,data}) => (
+  <View style={styles.item}>
+    <View style={styles.esquerda}>
+      <View style={styles.pagamen}>
+        <Image source={require('../../../assets/img/carteira/pix.png')} style={styles.foto}/>
+      </View>
+      <View style={styles.meio}>
+        <Text style={styles.passagem}>{passagem}</Text>
+        <Text style={styles.linha}>{linha}</Text>
+      </View>
+    </View>
+
+    <View style={styles.direita}>
+      <Text style={styles.data}>{data}</Text>
+    </View>
+  </View>
+);
+
+export default function Home() {
+  const navigation = useNavigation();
+
+  return (
+    <SafeAreaView style={styles.safeContainer}>
+      <View style={styles.container}>
+      <StatusBar barStyle="light-content"/>
+
+        <View style={[styles.cima, styles.shadow]}>
+          <View style={styles.nav}>
+              <Text style={styles.nomeCarteira}>CARTEIRA</Text>
+          </View>
+
+          <View style={styles.passagens}>
+            <Text style={styles.tituPassag}>Passagens disponiveis</Text>
+            <Text style={styles.qtdPassag}>42</Text>
+          </View>
+        </View>
+      
+
+        <View style={styles.recarga}>
+            <Text style={styles.titulo}>Recarga</Text>
+            <View style={styles.fundometodos}>
+                <View style={styles.circleMetodo}>
+                    <TouchableOpacity onPress={() => navigation.navigate('Config')}>
+                        <Image
+                            source={require('../../../assets/img/carteira/pix.png')}
+                            style={styles.metodo}
+                        />
+                    </TouchableOpacity>
                 </View>
-            </ScrollView>
-            <StatusBar barStyle="light-conten"/>
-        </SafeAreaView>
-    );
+                <View style={styles.circleMetodo}>
+                    <TouchableOpacity onPress={() => navigation.navigate('Config')}>
+                        <Image
+                            source={require('../../../assets/img/carteira/pix.png')}
+                            style={styles.metodo}
+                        />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.circleMetodo}>
+                    <TouchableOpacity onPress={() => navigation.navigate('Config')}>
+                        <Image
+                            source={require('../../../assets/img/carteira/pix.png')}
+                            style={styles.metodo}
+                        />
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+
+
+        <View style={styles.historico}>
+          <Text style={styles.titulo}>Historico</Text>
+          <FlatList
+            data={DATA}
+            renderItem={({item}) => <Item passagem={item.passagem} linha={item.linha} data={item.data}/>}
+            keyExtractor={item => item.id}
+          />
+        </View>
+
+      </View>
+    </SafeAreaView>
+  );
 }
--- src/pages/Carteira/styles.js
+++ src/pages/Carteira/styles.js
@@ -1,22 +1,161 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
+    safeContainer: {
+        flex:1,
+        backgroundColor: 'red',
+
+      },
     container: {
         flex: 1,
+        backgroundColor: '#fff',
         justifyContent: 'center',
         flexDirection: 'column',
-        backgroundColor: 'red',
-    },
-
-    nav: {
-        
-        backgroundColor: 'blue',
-    },
-
-    recarga: {
+      },
+      //PARTE VERMELHA
+      cima: {
         flex: 3,
-        backgroundColor: 'blue',
-    },
+        backgroundColor: 'red',
 
+        paddingHorizontal: 20,
+        marginBottom: 10,
+        borderBottomLeftRadius: 40,
+        borderBottomRightRadius: 40,
+      },
 
+      nav: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      nomeCarteira: {
+        color: 'white',
+        fontSize: 30,
+        fontWeight: 'bold',
+      },
+
+      passagens: {
+        flex: 2,
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+      },
+      tituPassag: {
+        color: 'white',
+        fontSize: 20,
+        fontWeight: 'bold',
+      },
+      qtdPassag: {
+        color: 'white',
+        fontSize: 60,
+        fontWeight: 'bold',
+      },
+
+      
+    
+      recarga: {
+        flex: 2,
+        paddingHorizontal: 20,
+      },
+
+      fundometodos: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+      },
+      circleMetodo: {
+        width: 70,
+        height: 70,
+        borderRadius: 50,
+        borderWidth: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      metodo: {
+        width: 50,
+        height: 50,
+        borderRadius: 50,
+      },
+
+      shadow: {
+        shadowColor: '#333333',
+        shadowOffset: {
+          width: 0,
+          height: 0,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 4,
+      },
+
+      
+      tituInte: {
+        fontSize: 20,
+        fontWeight: '600',
+        marginBottom: 7,
+        alignItems: 'flex-start',
+      },
+      tempo: {
+        fontSize: 50,
+        fontWeight: 'bold',
+      },
+    
+      historico: {
+        flex: 3,
+        paddingHorizontal: 20, 
+      },
+      titulo: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginBottom: 5,
+      },
+    
+    
+      item: {
+        flexDirection: 'row',
+        borderBottomWidth: 1,
+        borderColor: "thistle",
+        paddingVertical: 20,
+
+        justifyContent: 'space-between',
+      },
+
+      esquerda: {
+        rowGap: 5,
+        columnGap: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+      },
+      meio:{
+        //flexDirection: 'column', já é por padrao
+
+      },
+      pagamen: {
+        width: 40,
+        height: 40,
+        borderRadius: 50,
+        borderWidth: 2,
+        justifyContent: 'center',
+      },
+      foto: {
+        alignSelf: 'center',
+        width: 28,
+        height: 28,
+      },
+      passagem: {
+        fontSize: 20,
+        fontWeight: 'bold',
+      },
+      linha: {
+        fontSize: 13,
+        fontWeight: 'bold',
+        color: 'grey',
+      },
+
+      direita:{
+        
+      },
+      data: {
+        fontSize: 13,
+        fontWeight: 'bold',
+        color: 'grey',
+        
+      },
 });
