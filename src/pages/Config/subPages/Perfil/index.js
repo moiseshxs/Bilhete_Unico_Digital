a31544c1bd38071e77172cb0f709eecaa7f821cc
hugo <carvalhohugo425@gmail.com>
2024-01-23 19:22:41 -0300

    tela de perfil pronta, teste de abrir galeria ao apertar o botao

--- src/pages/Config/subPages/Perfil/index.js
+++ src/pages/Config/subPages/Perfil/index.js
@@ -1,11 +1,105 @@
-import React from 'react';
-import { Text, View, SafeAreaView} from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, SafeAreaView, Image, TouchableOpacity} from 'react-native';
 import styles from './styles';
-
+import Cassio from '../../../../../assets/img/home/perfil.png'
+import { Ionicons } from '@expo/vector-icons';
+import { launchImageLibrary } from "react-native-image-picker";
 export default function Perfil() {
+    
+    
+    const [file, setFile] = useState();
+
+    const handleChoosePhoto = () => {
+        const options = {
+          noData: true,
+          title: "Foto de avaliação",
+          takePhotoButtonTitle: "Escolha uma foto",
+          chooseFromLibraryButtonTitle: "Selecione da galeria uma foto",
+          selectionLimit: 1, // Se deixar 1, será permitido apenas uma foto e 0 várias
+        };
+    
+        launchImageLibrary(options, async (response) => {
+          if (response.didCancel) {
+            console.log("Usuário cancelou a seleção");
+          } else if (response.error) {
+            console.log("Ocorreu um erro.");
+          } else {
+            const photoFile = {
+              uri: asset.uri,
+              name: asset.fileName,
+              type: "image/jpeg",
+            };
+    
+            setFile(photoFile);
+          }
+        });
+      };
+
+
+
+
     return (
         <SafeAreaView style={styles.container}>
-            <View><Text>Perfil</Text></View>
+            <View style={styles.fotoArea}>
+                <View style={styles.fotoPlace}>
+                    <Image
+                    source={Cassio}
+                    style={styles.foto}/>
+                    <TouchableOpacity style={styles.icon}
+                    onPress={() => handleChoosePhoto}>
+                    <Ionicons
+                    name='camera'
+                    size={30}
+                    color='white'
+                    style={styles.icon}/>
+                    </TouchableOpacity>
+                </View>
+            </View>
+            <View style={styles.infosArea}>
+                <View style={styles.alterarArea}>
+                    <View>
+                    <Text style={{fontSize:20}}>Dados Pessoais</Text>
+                    </View>
+                    <TouchableOpacity>
+                    <Ionicons
+                    name='create'
+                    size={30}/>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.infos}>
+                    
+                    <View style={styles.infoController}>
+                        <Text style={styles.title}>Nome completo</Text>
+                        <Text style={styles.desc}>Cassio Adalberto Ramos</Text>
+                    </View>
+                    <View style={styles.infoController}>
+                        <Text style={styles.title}>E-mail</Text>
+                        <Text style={styles.desc}>gigantecassio@gmail.com</Text>
+                    </View>
+                    <View style={styles.infoController}>
+                        <Text style={styles.title}>Celular</Text>
+                        <Text style={styles.desc}>(11) 9 0000-0000</Text>
+                    </View>
+                    <View style={styles.infoController}>
+                        <Text style={styles.title}>CPF</Text>
+                        <Text style={styles.desc}>777.***.***-*7</Text>
+                    </View>
+                </View>
+            </View>
+            <View style={styles.dashArea}>
+                <View style={styles.dashController}>              
+                    <View style={styles.quadrado}>
+                        <Text style={styles.acao}>Consumos</Text>
+                        <Text style={styles.numero}>19</Text>
+                    </View>
+                    <View style={styles.quadrado}>
+                        <Text style={styles.acao}>Compras</Text>
+                        <Text style={styles.numero}>7</Text>
+                    </View>
+                </View>
+
+            </View>
+
         </SafeAreaView>
     );
 }
--- src/pages/Config/subPages/Perfil/styles.js
+++ src/pages/Config/subPages/Perfil/styles.js
@@ -3,8 +3,101 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
+        
         flexDirection: 'column',
         backgroundColor: '#fff',
     },
+    fotoArea:{
+        width:'100%',
+        height:'30%',
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    
+    foto:{
+        width:150,
+        height:150,
+        objectFit:'contain',
+        borderRadius:100
+    },
+    icon:{
+        position:'absolute',
+        right:'2%',
+        top:'80%',
+        color:'red',
+        
+        
+        borderRadius:100
+    },
+    infosArea:{
+        height:'45%',
+        justifyContent:'space-between',
+        alignItems:'center',
+    },
+    alterarArea:{
+        height:'15%',
+        width:'90%',
+        borderColor:'#7b7b7b',
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center',
+        borderBottomWidth:2,
+    },
+    infos:{
+        height:'78%',
+        width:'95%',
+        borderColor:'#7b7b7b',
+        borderWidth:2,
+        borderRadius:25,
+        justifyContent:'center',
+        alignItems:'center',
+        gap:5,
+        
+    },
+    infoController:{
+        width:'90%',
+        height:'20%',
+        flexDirection:'column',
+        justifyContent:'flex-start',
+    },
+    dashArea:{
+        width:'100%',
+        height:'20%',
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    dashController:{
+       width:'80%',
+       flexDirection:'row',
+       height:'100%',
+       justifyContent:'space-around',
+       alignItems:'center',
+    },
+    title:{
+        fontSize:16,
+        fontWeight:'500',
+    },
+    desc:{
+        fontSize:14,
+        color:'#7b7b7b'
+    },
+    quadrado:{
+        justifyContent:'center',
+        alignItems:'center',
+        height:'70%',
+        width:'40%',
+        borderWidth:2,
+        borderColor:'#7b7b7b',
+        borderRadius:25,
+        gap:8,
+    },
+    acao:{
+        fontSize:18,
+    },
+    numero:{
+        fontSize:16,
+        fontWeight:'700'
+    }
+    
 });
