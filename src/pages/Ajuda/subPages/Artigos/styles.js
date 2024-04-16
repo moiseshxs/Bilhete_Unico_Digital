ded89fa0fe8e3f7b36e22750b99db819360f9291
moiseshxs <moiseshenryb@gmail.com>
2024-04-16 20:29:57 -0300

    Tela de artigos(por enquanto fica isso)

--- src/pages/Ajuda/subPages/Artigos/styles.js
+++ src/pages/Ajuda/subPages/Artigos/styles.js
@@ -5,14 +5,14 @@ const Height = Dimensions.get('screen').height;
 export default StyleSheet.create({
     container: {
         flex: 1,
-        padding: Width/40,
+        padding: Width / 40,
         flexDirection: 'column',
         backgroundColor: '#fff',
 
     },
-    center:{
-        width: Width/2.3,
-        margin: Width/40,
+    center: {
+        width: Width / 2.3,
+        margin: Width / 40,
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'space-between',
@@ -25,35 +25,36 @@ export default StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
-        marginBottom: Height/40
+        marginBottom: Height / 40
     },
-    boxTitulo:{
-
-        gap: Height/30
+    boxTitulo: {
+        paddingHorizontal: 20,
+        gap: Height / 30
     },
     subTitulo: {
 
         fontWeight: 'bold',
         fontSize: 28,
-        width: Width/1.35,
-        gap: Height/2
+        width: Width / 1.35,
+        gap: Height / 2
 
     },
     caminho: {
         fontWeight: 'bold',
     },
 
-    boxQuestionario:{
+    boxQuestionario: {
         flex: 1,
-        justifyContent: 'center'
+        paddingHorizontal: 20,
+        justifyContent: 'center',
     },
-    artigoUtil:{
-        marginVertical: Width/50,
+    artigoUtil: {
+        marginVertical: Width / 50,
         fontSize: 17,
     },
-    buttonAjuda:{
-        height: Height/20,
-        width: Width/5.3,
+    buttonAjuda: {
+        height: Height / 20,
+        width: Width / 5.3,
         borderRadius: 50,
         borderWidth: 1.5,
         borderColor: '#000',
@@ -63,9 +64,9 @@ export default StyleSheet.create({
         alignItems: 'center',
 
     },
-    linha:{
+    linha: {
         borderBottomWidth: 1,
-        borderColor: '#000', 
+        borderColor: '#000',
         display: 'flex',
         justifyContent: 'space-between'
     }
