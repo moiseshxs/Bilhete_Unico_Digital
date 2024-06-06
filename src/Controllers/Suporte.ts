2898e4ff9a577b855ce34d6e62b7542f8838f14d
Mateus707 <mateuss3553@gmail.com>
2024-06-06 17:36:10 -0300

    duvidas frequentes

--- src/Controllers/Suporte.ts
+++ src/Controllers/Suporte.ts
@@ -2,14 +2,16 @@ import Api from "../Services/api/Api";
 
 
 class Suporte extends Api{
-    async storeSuporte(token:string, categoriaSuporte:string, descSuporte:string) {
+    async storeSuporte(token:string,categoriaSuporte:string, descSuporte:string,id_passageiro:string) {
         try {
             let form = new FormData();
+            form.append("tipoAcao", 'Suporte');
             form.append("categoriaSuporte", categoriaSuporte);
             form.append("descSuporte", descSuporte);
+            form.append("statusSuporte", 'Aberto');
     
-            const response = await this.api.post(`/storeVoto`, form, { headers: {'Authorization': `Bearer ${token}`}});
-    
+            const response = await this.api.post(`/acao/${id_passageiro}`, form, { headers: {'Authorization': `Bearer ${token}`}});
+
             return response.data;
         } catch (error) {
             console.log(error, "ERRO AO inserir");
--- src/pages/Ajuda/subPages/FaleComFuncionario/index.js
+++ src/pages/Ajuda/subPages/FaleComFuncionario/index.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import { Text, View, SafeAreaView, TouchableOpacity, Dimensions, ScrollView, TextInput, Pressable } from 'react-native';
+import React, { useState, useContext } from 'react';
+import { Text, View, SafeAreaView, TouchableOpacity, TextInput, Pressable } from 'react-native';
 import styles from './styles';
-import { FloatingLabelInput } from "react-native-floating-label-input";
-import { Ionicons } from '@expo/vector-icons';
-import { useState } from "react";
-import { useNavigation } from '@react-navigation/native';
-
-
-
+import CreateSuporte from '../../../../Controllers/Suporte';
+import MyContext from '../../../../Context/context';
 
 export default function FaleComFuncionario() {
+    const { token, passageiro } = useContext(MyContext);
 
-    const navigation = useNavigation();
-    const [assunto, setAssunto] = useState()
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [tipoSuporte, setTipoSuporte] = useState('');
+    const [mensagem, setMensagem] = useState('');
+
+    const create = async () => {
+        const c = new CreateSuporte();
+        const response = await c.storeSuporte(token, tipoSuporte, mensagem, passageiro.id);
+        if(response){
+            setNome("")
+            setEmail("")
+            setTipoSuporte("")
+            setMensagem("")
+        }
+        // Faça algo com a resposta, se necessário
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.areaHeader}>
@@ -26,97 +37,40 @@ export default function FaleComFuncionario() {
                     <Text style={styles.textInput}>NOME*</Text>
                     <TextInput
                         style={styles.input}
+                        value={nome}
+                        onChangeText={setNome}
                     />
                 </View>
                 <View>
                     <Text style={styles.textInput}>E-MAIL*</Text>
                     <TextInput
                         style={styles.input}
+                        value={email}
+                        onChangeText={setEmail}
                     />
                 </View>
                 <View>
                     <Text style={styles.textInput}>TIPO DE SUPORTE*</Text>
                     <TextInput
                         style={styles.input}
+                        value={tipoSuporte}
+                        onChangeText={setTipoSuporte}
                     />
                 </View>
                 <View>
                     <Text style={styles.textInput}>MENSAGEM*</Text>
                     <TextInput
                         style={styles.inputMsg}
+                        value={mensagem}
+                        onChangeText={setMensagem}
                     />
                 </View>
             </View>
             <View style={styles.areaBtn}>
-                <Pressable style={styles.btnEnviar}>
+                <Pressable style={styles.btnEnviar} onPress={create}>
                     <Text style={styles.textBtn}>Enviar</Text>
                 </Pressable>
             </View>
         </SafeAreaView>
     );
 }
-
-{/* <ScrollView>
-                <View style={styles.container}>
-                    <View style={styles.section}>
-                        <View style={styles.boxAssunto}>
-
-                            <Text style={styles.assunto}> Qual é o erro?</Text>
-                            <Text style={styles.textAssunto}>Nos informe detalhes sobre o erro e
-                                anexe prints do erro para te ajudar</Text>
-                        </View>
-                        <FloatingLabelInput
-                            label='Informe o erro'
-                            staticLabel
-                            hintTextColor='#aaa'
-                            multiline={true}
-                            numberOfLines={10}
-                            value={assunto}
-                            placeholder='Escrever e-mail'
-                            containerStyles={{
-                                borderWidth: 2,
-                                minWidth: Dimensions.get('screen').width / 1.15,
-                                paddingHorizontal: 10,
-                                borderColor: '#f00',
-                                borderRadius: 40,
-                            }}
-                            customLabelStyles={{
-                                fontSizeFocused: 12,
-                                color: '#9B9B9B',
-                            }}
-                            labelStyles={{
-                                backgroundColor: '#fff',
-                                paddingHorizontal: 8,
-                                lineHeight: 15,
-                                fontSize: 16,
-                                fontWeight: '500'
-                            }}
-                            inputStyles={{
-                                color: 'black',
-                                borderColor: 'transparent',
-                                paddingHorizontal: 10,
-                                fontSize: 16
-                            }}
-
-                            onChangeText={value => {setAssunto(value)}}
-                   
-                        />
-                        <View style={styles.boxButton}>
-
-                            <View style={styles.buttonArea}>
-                                
-                                    <TouchableOpacity style={styles.buttonAjuda}>
-                                        <Ionicons name="document-text-outline" size={30}></Ionicons>
-                                    </TouchableOpacity>
-                                    <TouchableOpacity style={styles.buttonAjuda}>
-                                        <Ionicons name="camera-outline" size={30}></Ionicons>
-                                    </TouchableOpacity>
-                                
-                                <TouchableOpacity style={styles.buttonAjuda}>
-                                    <Ionicons name="paper-plane-outline" size={30}></Ionicons>
-                                </TouchableOpacity>
-                            </View>
-                        </View>
-                    </View>
-                </View>
-            </ScrollView> */}
